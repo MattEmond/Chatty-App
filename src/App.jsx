544c03ee9916457161cf98d9ee87b2ea065b3926
Matthew Emond <matthewemond@Matthews-MacBook-Pro.local>
2017-11-28 12:26:36 -0500

    Initial state set; currentUser set using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React, {Component} from 'react';
-import ChatBar from './ChatBar.jsx'
-import MessageList from './MessageList.jsx'
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
+
+}
+
   render() {
     console.log("Rendering <App/>");
     return (
@@ -10,8 +29,8 @@ class App extends Component {
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <MessageList />
-        <ChatBar />
+        <MessageList message={this.state.message} />
+        <ChatBar user={this.state.currentUser.name} />
       </div>
     );
   }
