10e1be59b935d9a36ebf0077e051c60052f9d5f6
Matthew Emond <matthewemond@Matthews-MacBook-Pro.local>
2017-11-27 21:05:47 -0500

    Set up jsx files to replace HTML

---
+++ src/Message.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <span class="message-username">Anonymous1</span>
+      <span class="message-content">I wont be impressed with technology until I can download food.</span>
+    );
+  }
+}
+export default Message;
