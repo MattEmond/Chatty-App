f97abc24927e1437689a8a2bd4c90b6beac621c0
Matthew Emond <matthewemond@Matthews-MacBook-Pro.local>
2017-11-28 10:02:08 -0500

    Code updated from HTML file into various JSX files

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,8 +3,15 @@ import React, {Component} from 'react';
 class Message extends Component {
   render() {
     return (
-      <span class="message-username">Anonymous1</span>
-      <span class="message-content">I wont be impressed with technology until I can download food.</span>
+      <div>
+        <div className="message" >
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">I wont be impressed with technology until I can download food.</span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </div>
     );
   }
 }
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,9 +3,8 @@ import React, {Component} from 'react';
 class MessageList extends Component {
   render() {
     return (
-      <div class="message system">
-        Anonymous1 changed their name to nomnom.
-      </div>
+      <main>
+      </main>
     );
   }
 }
